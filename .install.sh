24f2f792bd64fe1a389bf379ecd268b17b565367
Mohamad Radisha <pr0ph0z23@gmail.com>
2024-10-29 20:29:24 +0700

    feat: Add macos settings

--- .install.sh
+++ .install.sh
@@ -9,3 +9,9 @@ brew tap FelixKratz/formulae
 brew install sketchybar
 
 brew services start sketchybar
+
+defaults write com.apple.dock workspaces-edge-delay -float 0.1
+defaults write com.apple.dock autohide -bool true
+defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
+defaults write com.apple.finder DisableAllAnimations -bool true
+defaults write -g NSScrollViewRubberbanding -int 0

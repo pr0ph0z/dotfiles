8ae3680a156d917d4d2a1a8bd59f18bd7446c1e8
Mohamad Radisha <pr0ph0z23@gmail.com>
2024-11-07 19:46:08 +0700

    feat: Add emoji suggestion removal

--- .install.sh
+++ .install.sh
@@ -21,3 +21,4 @@ defaults write com.apple.dock autohide -bool true
 defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
 defaults write com.apple.finder DisableAllAnimations -bool true
 defaults write -g NSScrollViewRubberbanding -int 0
+sudo defaults write /Library/Preferences/FeatureFlags/Domain/UIKit.plist emoji_enhancements -dict-add Enabled -bool NO
